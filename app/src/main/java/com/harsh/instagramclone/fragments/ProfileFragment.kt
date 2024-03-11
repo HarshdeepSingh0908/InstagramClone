668a830d29ddab09ffd346c077a42083c6de797a
HarshdeepSingh0908 <harsh3792002@gmail.com>
2024-03-11 23:14:30 +0530

    layout changes

--- app/src/main/java/com/harsh/instagramclone/fragments/ProfileFragment.kt
+++ app/src/main/java/com/harsh/instagramclone/fragments/ProfileFragment.kt
@@ -37,10 +37,10 @@ class ProfileFragment : Fragment() {
 
         val list: MutableList<HighLightData> = mutableListOf()
         list.add(HighLightData("home", R.drawable.user1))
-        list.add(HighLightData("friends", R.drawable.user1))
-        list.add(HighLightData("travel", R.drawable.user1))
-        list.add(HighLightData("cooking", R.drawable.user1))
-        list.add(HighLightData("work", R.drawable.user1))
+        list.add(HighLightData("friends", R.drawable.user2))
+        list.add(HighLightData("travel", R.drawable.user3))
+        list.add(HighLightData("cooking", R.drawable.user4))
+        list.add(HighLightData("work", R.drawable.user5))
         binding.rvHighlights.adapter = HighlightAdapter(list)
         val layoutManager1 = LinearLayoutManager(
             requireContext(),
@@ -51,23 +51,20 @@ class ProfileFragment : Fragment() {
             ProfileImageData(R.drawable.user1),
             ProfileImageData(R.drawable.user2),
             ProfileImageData(R.drawable.user3),
-            ProfileImageData(R.drawable.user1),
-            ProfileImageData(R.drawable.user2),
-            ProfileImageData(R.drawable.user3),
-            ProfileImageData (R.drawable.user1),
-            ProfileImageData(R.drawable.user2),
-            ProfileImageData(R.drawable.user3),
-            ProfileImageData(R.drawable.user1),
-            ProfileImageData(R.drawable.user2),
-            ProfileImageData(R.drawable.user3)
+            ProfileImageData(R.drawable.user4),
+            ProfileImageData(R.drawable.user5),
+            ProfileImageData(R.drawable.user6),
+            ProfileImageData (R.drawable.user7),
+            ProfileImageData(R.drawable.user8),
+            ProfileImageData(R.drawable.user9),
+            ProfileImageData(R.drawable.user10)
+
         )
 
         val layoutManager = GridLayoutManager(requireContext(), 3)
         binding.rvProfileImages.layoutManager = layoutManager
-
-        // Set up adapter for RecyclerView
-        val adapter = ProfileImageAdapter(profileImages)
-        binding.rvProfileImages.adapter = adapter
+        val postadapter = ProfileImageAdapter(profileImages)
+        binding.rvProfileImages.adapter = postadapter
 
     }
 
